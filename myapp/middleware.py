211cfc4f2612a52c140ded02ec886bcfe40e35dd
Balaji45-Ncr <ncrlpb45@gmail.com>
2025-06-18 23:11:25 +0530

    unauth_middlware

--- myapp/middleware.py
+++ myapp/middleware.py
@@ -2,18 +2,38 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import redirect,reverse
 class RedirectAuthenticatedUserMiddleware:
-    def __init__(self,get_response):
-        self.request=get_response
+    def __init__(self, get_response):
+        self.get_response = get_response
 
-    def __call__(self,request):
-        #checks if user is authenticated or not
-        if request.user.is_authenticated:
-            redirect_urls=[
-                reverse('blog:login'),reverse('blog:register')
-            ]
-            if request.path in redirect_urls:
-               return redirect(reverse('blog:dashboard'))
-            response=self.request(request)
-            return response
+    def __call__(self, request):
+        redirect_urls = [
+            reverse('blog:login'),
+            reverse('blog:register'),
+        ]
 
+        # If the user is authenticated and trying to access login/register
+        if request.user.is_authenticated and request.path in redirect_urls:
+            return redirect(reverse('blog:dashboard'))
+
+        # Always call the next middleware/view and return response
+        response = self.get_response(request)
+        return response
+
+
+class RedirectUnAuthenticatedUserMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+        protected_urls = [
+            reverse('blog:dashboard'),
+        ]
+
+        # If not logged in and trying to access protected URL
+        if not request.user.is_authenticated and request.path in protected_urls:
+            return redirect(reverse('blog:login'))
+
+        # Always call the next middleware/view and return response
+        response = self.get_response(request)
+        return response
 
--- myapp/settings.py
+++ myapp/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'myapp.middleware.RedirectAuthenticatedUserMiddleware',
+    'myapp.middleware.RedirectUnAuthenticatedUserMiddleware',
 
 ]
 
